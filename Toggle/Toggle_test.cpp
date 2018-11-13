ccac6c4e6b035204e1d148c9071e220feed1b098
linhsu0723 <linhsu0723@qq.com>
2018-11-14 01:49:27 +0800

    key combination, timer

--- Toggle/Toggle_test.cpp
+++ Toggle/Toggle_test.cpp
@@ -2,6 +2,7 @@
 using namespace cpp_utils;
 
 #include <iostream>
+#include <chrono>
 using namespace std;
 
 #include <GLFW/glfw3.h>
@@ -27,31 +28,50 @@ void TerminateGLFW(GLFWwindow *window) {
 	glfwTerminate();
 }
 
-// Tests on GLFW key events.
+
 int main() {
+// Tests on GLFW key events.
 	auto window = InitGLFW("Toggle_test", 512, 384);
 
-	Toggle toggle_s = Toggle(true, [&window]() {
-		return glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS;
-	}, [](const bool &state) {
-		cout << "Key S pressed. State changes to " << state << endl;
-	});
-
+	// Key: F
 	Toggle toggle_f = Toggle(true, [&window]() {
 		return glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS;
 	}, [](const bool &state) {
-		cout << "Off2On(): Key F pressed. State changes to " << state << endl;
+		cout << "Key F pressed. State changes to " << state << endl;
+	});
+
+	// Key combination: Ctrl+Shift+C
+	Toggle toggle_ctrl_shift_c = Toggle(true, [&window]() {
+		return glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS
+			&& glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS
+			&& glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS;
 	}, [](const bool &state) {
-		cout << "On2Off(): Key F pressed. State changes to " << state << endl;
+		cout << "Off2On(): Key Ctrl+Shift+C pressed. State changes to " << state << endl;
+	}, [](const bool &state) {
+		cout << "On2Off(): Key Ctrl+Shift+C pressed. State changes to " << state << endl;
 	});
 
 	while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && !glfwWindowShouldClose(window)) {
-		toggle_s.Update();
 		toggle_f.Update();
+		toggle_ctrl_shift_c.Update();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	TerminateGLFW(window);
+
+// Tests on std::chrono timer.
+	auto start = std::chrono::steady_clock::now();
+	int second = 0;
+	Toggle toggle_alarm = Toggle(true, [&start, &second]() {
+		std::chrono::duration<double> diff = std::chrono::steady_clock::now() - start;
+		return diff.count() > second;
+	}, [&second](const bool &state) {
+		cout << second << " second(s). State changes to " << state << endl;
+		second++;
+	});
+	while(second < 10) {
+		toggle_alarm.Update();
+	}
 }
