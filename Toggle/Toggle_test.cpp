d8c78220add416f849ab8baedd5c314074dd5ebc
linhsu0723 <linhsu0723@qq.com>
2019-07-12 15:02:52 +0800

    GLFW.hpp

--- Toggle/Toggle_test.cpp
+++ Toggle/Toggle_test.cpp
@@ -1,32 +1,11 @@
 #include "Toggle.hpp"
 using namespace cpp_utils;
 
+#include "GLFW.hpp"
+
 #include <iostream>
 #include <chrono>
 using namespace std;
-
-#include <GLFW/glfw3.h>
-
-GLFWwindow* InitGLFW(const char *window_title, int window_w, int window_h) {
-	if(!glfwInit()) exit(EXIT_FAILURE);
-	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
-
-	GLFWwindow* window = glfwCreateWindow(window_w, window_h, window_title, NULL, NULL);
-	if(!window) {
-		glfwTerminate();
-		fprintf(stderr, "Failed to create GLFW window\n");
-		exit(EXIT_FAILURE);
-	}
-	glfwMakeContextCurrent(window);
-
-	return window;
-}
-
-void TerminateGLFW(GLFWwindow *window) {
-	glfwDestroyWindow(window);
-	glfwTerminate();
-}
 
 int main() {
 // Tests on GLFW key events.
