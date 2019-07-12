36ddeeb925d253271792b2b40b1556abb15785fe
linhsu0723 <linhsu0723@qq.com>
2019-07-12 15:34:01 +0800

    Toggle.hpp: comments

--- Toggle/Toggle.hpp
+++ Toggle/Toggle.hpp
@@ -8,7 +8,7 @@ namespace cpp_utils {
 // A toggle simulator.
 // Toggle state is ON(true) or OFF(false).
 // Toggle can be switched from one state to another.
-// Actions can be set which will be taken when (just after) state changes.
+// Actions can be set to be taken when the toggle is switched (just after state changes).
 class Toggle {
 	const std::function<bool()> switched_;
 	bool state_;
@@ -27,10 +27,10 @@ public:
 		assert(pressed != nullptr);
 	}
 
-	// Actions taken when (just after) state_ changes from OFF to ON and from ON to OFF.
-	// on2off will be same as off2of if it's nullptr: call the same function when (just after)
-	// state_ changes from ON to OFF and from OFF to ON.
-	// Both of them could be nullptr: no action taken when state_ changes.
+	// Actions (`off2on()` and `on2off()`) will be taken
+	// when the toggle is switched (just after state changes from OFF to ON and from ON to OFF.
+	// `on2off()` will be same as `off2of()` if it's `nullptr`: the same action will be taken.
+	// Both of them could be `nullptr`: no action taken when the toggle is switched.
 	bool Update(
 		const std::function<void(bool)> &off2on = nullptr,
 		const std::function<void(bool)> &on2off = nullptr
@@ -38,14 +38,14 @@ public:
 		if(count_-- > 0) return state_;
 		if(switched_()) {
 			if(!switching_) {
-				// state_ changes just before actions taken.
+				// `state_` changes just before actions taken.
 				state_ = !state_;
 				if(state_) {
-					// Call off2on() when state_ changes from off(false) to on(true).
+					// Call `off2on()` when `state_` changes from off(false) to on(true).
 					if(off2on) off2on(state_);
 				} else {
-					// Call on2off() when state_ changes from on(true) to off(false).
-					// Or, if it is nullptr, take the same action as off2on().
+					// Call `on2off()` when `state_` changes from on(true) to off(false).
+					// Or, if it is `nullptr`, take the same action as `off2on()`.
 					if(on2off)
 						on2off(state_);
 					else if(off2on)
